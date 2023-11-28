4737aa570e57b4b3c08b824ae37aff0f7ca01fc7
Michael Johnson <michaeljcantley@gmail.com>
2023-11-28 15:43:35 -0500

    Finish initial page load

--- src/index.js
+++ src/index.js
@@ -1,13 +1,114 @@
 import './styles/style.sass';
 
 const content = document.querySelector('.content');
-const areaChoice = document.querySelector('.areaChoice')
-const tempInfo = document.querySelector('.temperatureInfo')
-const forecast = document.querySelector('.forecast');
 
-const showWeather = () => {
-    tempInfo.setAttribute('data-weather', '');
-    forecast.setAttribute('data-weather', '');
+window.onload = loadInitialContent()
+
+function createNav() {
+    // Creates the header and all of the headers content
+    const header = document.createElement('header');
+    content.append(header)
+
+    const nav = document.createElement('nav');
+
+    const navBar = document.createElement('div');
+    navBar.classList.add('navbar')
+
+    const title = document.createElement('h1');
+    title.textContent = 'Temperate WeatherApp';
+    navBar.append(title);
+
+    const hamburger = document.createElement('div');
+    hamburger.classList.add('hamburger');
+    for (let i = 0; i < 3; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        hamburger.append(line);
+    }
+    navBar.append(hamburger);
+
+    const links = document.createElement('ul');
+    links.classList.add('links');
+    
+    const linkContainer1 = document.createElement('li');
+    const link1 = document.createElement('a');
+    link1.textContent = 'Home';
+    linkContainer1.append(link1);
+    links.append(linkContainer1);
+
+    const linkContainer2 = document.createElement('li');
+    const link2 = document.createElement('a');
+    link2.textContent = 'Forecast';
+    linkContainer2.append(link2);
+    links.append(linkContainer2);
+    
+
+    const linkContainer3 = document.createElement('li');
+    const link3 = document.createElement('a');
+    link3.textContent = 'Warnings';
+    linkContainer3.append(link3);
+    links.append(linkContainer3);
+
+    navBar.append(links)
+    nav.append(navBar);
+    header.append(nav);
 }
 
-showWeather()
+function createMainContent() {
+    // creates all of the main content on load
+    const main = document.createElement('main');
+    content.append(main);
+
+    const mainContent = document.createElement('div');
+    mainContent.classList.add('mainContent');
+    main.append(mainContent);
+
+    const zipContainer = document.createElement('div');
+    zipContainer.classList.add('zipContainer');
+    mainContent.append(zipContainer);
+
+    const zipCode = document.createElement('input');
+    zipCode.setAttribute('type', 'text');
+    zipCode.name = 'zipCode';
+    zipCode.id = 'zipCode';
+    zipCode.placeholder = 'Insert Zip Here';
+    zipContainer.append(zipCode);
+
+    const searchButton = document.createElement('button');
+    searchButton.setAttribute('type', 'button');
+    searchButton.textContent = 'Search';
+    zipContainer.append(searchButton);
+
+    const areaChoice = document.createElement('div');
+    areaChoice.classList.add('areaChoice');
+    mainContent.append(areaChoice);
+
+    const temperatureInfo = document.createElement('div');
+    temperatureInfo.classList.add('temperatureInfo');
+    mainContent.append(temperatureInfo);
+
+    const forecast = document.createElement('div');
+    forecast.classList.add('forecast');
+    mainContent.append(forecast);
+}
+
+function createFooter() {
+    const footer = document.createElement('footer');
+    content.append(footer);
+
+    const attribution = document.createElement('a');
+    attribution.href = 'https://github.com/mikej321';
+    attribution.textContent = 'Michael Johnson';
+
+    const footerContent = document.createElement('p');
+    footerContent.textContent = `Made by `;
+    footerContent.insertAdjacentElement('beforeend', attribution);
+
+    footer.append(footerContent);
+}
+
+function loadInitialContent() {
+    createNav();
+    createMainContent();
+    createFooter();
+}
