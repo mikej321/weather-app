00a84f261a08453cf06f47a0420f2ca407c252c5
Michael Johnson <michaeljcantley@gmail.com>
2023-11-28 14:37:37 -0500

    Finish styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles/style.sass';
 
 const content = document.querySelector('.content');
+const areaChoice = document.querySelector('.areaChoice')
+const tempInfo = document.querySelector('.temperatureInfo')
+const forecast = document.querySelector('.forecast');
+
+const showWeather = () => {
+    tempInfo.setAttribute('data-weather', '');
+    forecast.setAttribute('data-weather', '');
+}
+
+showWeather()
